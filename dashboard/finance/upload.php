49975b717336faa71583fa0881b8beb166e2b9cc
CHIH-YANG HUANG <user@hcy.idv.tw>
2018-06-29 01:55:23 +0800

    Add with Create Record

--- dashboard/finance/upload.php
+++ dashboard/finance/upload.php
@@ -9,22 +9,46 @@
     } else{
         echo "There was an error uploading the file, please try again!";
     }
+    $uid = $_POST["uid"];
+    $name = $_POST["name"];
+    $date = $_POST["date"];;
+    $money = $_POST["money"];
+    $reason  = $_POST["reason"];
+    $item = $_POST["item"];
+    $file =  basename( $_FILES['uploaded_file']['name']);
+    echo "<br>";
+    echo $uid;
+    echo "<br>";
+    echo $name;
+    echo "<br>";
+    echo $date;
+    echo "<br>";
+    echo $money;
+    echo "<br>";
+    echo $reason;
+    echo "<br>";
+    echo $item;
+    echo "<br>";
 
-    echo "<br>";
-    echo $_POST["uid"];
-    echo "<br>";
-    echo $_POST["name"];
-    echo "<br>";
-    echo $_POST["date"];
-    echo "<br>";
-    echo $_POST["money"];
-    echo "<br>";
-    echo $_POST["reason"];
-    echo "<br>";
-    echo $_POST["item"];
-    echo "<br>";
+    $conn = new mysqli("localhost","hcy_utlms","utaipei8362","hcy_utlms");
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    mysqli_set_charset($conn,"utf8");
+    $sql = "INSERT INTO finance (eventid, uid, item, semester, mydate, mymoney, reason, status, filename) VALUES ( NULL,'$uid','$item','$semester','$date','$money','$reason','0','$file' )";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+
+
+
+
 }else{
-
 
     echo "ERROR";
 }
